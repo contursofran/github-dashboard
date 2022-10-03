92f338f309a4eea301d845321a7556ccf0354e78
fconturso <contursofran@gmail.com>
2022-10-03 14:05:12 -0300

    Add tooltips and percentages labels to TopLanguages

--- views/Overview/components/TopLanguages/TopLanguages.tsx
+++ views/Overview/components/TopLanguages/TopLanguages.tsx
@@ -59,22 +59,27 @@ function TopLanguages({ username }: { username: string | undefined }) {
             {
               value: languages[0].percentage ? languages[0].percentage : 0,
               color: languages[0]?.color ? languages[0].color : "gray",
+              tooltip: languages[0]?.name,
             },
             {
               value: languages[1].percentage ? languages[1].percentage : 0,
               color: languages[1]?.color ? languages[1].color : "gray",
+              tooltip: languages[1]?.name,
             },
             {
               value: languages[2].percentage ? languages[2].percentage : 0,
               color: languages[2]?.color ? languages[2].color : "gray",
+              tooltip: languages[2]?.name,
             },
             {
               value: languages[3].percentage ? languages[3].percentage : 0,
               color: languages[3]?.color ? languages[3].color : "gray",
+              tooltip: languages[3]?.name,
             },
             {
               value: languages[4].percentage ? languages[4].percentage : 0,
               color: languages[4]?.color ? languages[4].color : "gray",
+              tooltip: languages[4]?.name,
             },
           ]}
           size={9}
@@ -86,9 +91,13 @@ function TopLanguages({ username }: { username: string | undefined }) {
                 color={language.color ? language.color : "gray"}
                 size={12}
               />
-              <Text>{language.name}</Text>
+              <Text>{language.name + " " + language.percentage + "%"}</Text>
             </Group>
           ))}
+          <Group>
+            <ColorSwatch color={"#373a40"} size={12} />
+            <Text>{"Others"}</Text>
+          </Group>
         </SimpleGrid>
       </Stack>
     </Card>
